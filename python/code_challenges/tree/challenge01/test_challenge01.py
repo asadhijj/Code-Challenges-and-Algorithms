394772d66d5b8265cc7026e01227a5d1db1156c7
asadhijj <hijjawi30@gmail.com>
2022-11-05 23:00:06 +0200

    adding the white board with docstring

--- python/code_challenges/tree/challenge01/test_challenge01.py
+++ python/code_challenges/tree/challenge01/test_challenge01.py
@@ -2,6 +2,7 @@ import pytest
 from challenge01 import *
 
 def test_build_tree():
+    '''Testing Case 1'''
     tree = TreeBuilder()
     assert tree.buildTree([3,9,20,15,7],[9,3,15,20,7]).val == 3
     assert tree.buildTree([3,9,20,15,7],[9,3,15,20,7]).left.val == 9
@@ -10,10 +11,12 @@ def test_build_tree():
     assert tree.buildTree([3,9,20,15,7],[9,3,15,20,7]).right.right.val == 7
 
 def test_build_tree2():
+    '''Testing Case 2'''
     tree = TreeBuilder()
     assert tree.buildTree([-1],[-1]).val == -1
 
 def test_build_tree3():
+    '''Testing case 3'''
     tree = TreeBuilder()
     assert tree.buildTree([1,2,3],[2,1,3]).val == 1
     assert tree.buildTree([1,2,3],[2,1,3]).left.val == 2
